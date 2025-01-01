61809f4ff5058b1ad8e1449b43dfadaea081719c
chacham <ccm0702@naver.com>
2025-01-01 17:58:21 +0900

    2025.01.01. first blog

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,16 +1,15 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
+import LinkedinIcon from '@/components/icons/LinkedinIcon'
 
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/chacham',
 		icon: GithubIcon
 	},
-
 	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/changmin-choi-99971b148',
+		icon: LinkedinIcon
 	}
 ] as const
